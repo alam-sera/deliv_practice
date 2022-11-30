9c0aa7b37913e8c5b6e8b7b2e03560a43f8c0c29
Alex Lam <alam.seraph@gmail.com>
2022-11-29 16:06:17 -0800

    added practice problems

---
+++ doc1.rb
@@ -0,0 +1,72 @@
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+# x = 93
+# if x == 25
+#   p 0
+# else 
+#   p -1
+# end
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+# x = 8
+# if x < 10 
+#   p -1
+# elsif x > 10 
+#   p 1
+# elsif x == 10
+#   p 0
+# end 
+
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+# x = 40
+# if x > 80 
+#   p 5
+# elsif x > 60 && x < 80
+#   p 4
+# elsif x > 40 && x < 60
+#   p 3
+# elsif x > 20 && x < 40
+#   p 2
+# else
+#   p 1
+# end
+
+# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+# books = [{title: "Harry Potter", author: "JK Rowling"}, {title: "Count of Monte Cristo", author: "Alexander Dumas"}, {title: "Wild", author: "Cheryl Strayed"}]
+
+# p books[2][:author]
+
+# 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+# words = {"entice" => "attract or tempt", "confound" => "cause surprise" }
+
+# words["erogenous"] = "sensitive"
+
+# p words
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+# puts "Enter a word"
+# word = gets.chomp
+# x = word.length
+# puts x
+
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+# puts "Enter a word"
+# word = gets.chomp
+# if word.length > 5 
+#   p "That's a long word"
+# elsif 
+#   p word
+# end 
+
+#  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
+#     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg"
+loop1 = ["a", "b", "c"]
+loop2 = ["d", "e", "f", "g"]
+loop3 = []
+x = 0 
+while x < loop1.length
+  
+end 
